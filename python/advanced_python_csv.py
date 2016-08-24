571cf9a781274eda6fe2722f5d60c089d4e8a16e
mitchki <kim@who-knows.com>
2016-08-24 10:41:34 -0400

    adding write csv file exercise

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,30 @@
-PLACE YOUR CODE HERE
+#PLACE YOUR CODE HERE
+
+
+import csv
+
+def reg_email(data):
+    emails = []
+    with open(data) as f:
+    	reader = csv.reader(f)
+    	header_line = next(reader)
+    	for row in reader:
+    		emails.append(row[3])
+	f.close()
+	return emails
+
+def email_csv(ifil,ofil):
+	#emails.csv
+	e = reg_email(ifil)
+	#	print type(e)
+	ofile  = open(ofil, "w")
+
+	writer = csv.writer(ofile, delimiter=' ', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+	for row in e:
+	#	print row
+		writer.writerow([row])
+
+	ofile.close()
+	#	print 'done'
+
